d536ea5ece9e97abe57305bef4e0c6a0c8285c94
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-01-17 15:16:51 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex-center">
+    <header-dashboard :height="heightHeader" />
+    <div :style="`min-height:${contentHeight}px`" class="flex flex-center">dfdsfd</div>
+  </q-page>
+</template>
+
+<script setup>
+import HeaderDashboard from './dashboard/HeaderDashboard.vue'
+
+const height = window.innerHeight
+const heightHeader = height / 5
+const contentHeight = height - heightHeader
+console.log('index height', height)
+</script>
---
+++ src/pages/dashboard/HeaderDashboard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="bg-grey-2 relative-position" :style="`min-height:${height}px`">
+    <div class="row container items-center absolute-bottom  q-pb-lg q-pt-lg">
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <div class="row items-center">
+          <q-avatar size="100px" font-size="52px" color="grey" text-color="white" icon="directions" />
+          <div class="column q-ml-md">
+            <div class="text-h5 text-weight-bold">Nama Akun</div>
+            <div class="text-grey">Executive Account</div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <div class="text-right">
+          <div class="text-h5 text-weight-bold">Dashboard Executive</div>
+          <div class="text-grey">Dashboard ini Hanya bisa di akses Oleh Direktur</div>
+          <div class="text-primary">UOBK RSUD MOHAMAD SALEH</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  height: {
+    type: Number,
+    default: 100
+  }
+})
+</script>
+<style lang="scss" scoped>
+.container {
+  padding-left: 5%;
+  padding-right: 10%;
+}
+</style>
