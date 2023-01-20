14e761704801a77372c29c9ec4105494f46ef5e2
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-01-20 13:43:12 +0700

    simpan kantor

--- src/pages/ExecutivePage.vue
+++ src/pages/ExecutivePage.vue
@@ -5,12 +5,12 @@ import HeaderPage from './executive/HeaderPage.vue';
   <q-page class="flex flex-center">
     <div class="frame-dark">
       <HeaderPage :width="app.txt140" :key="app.txt140"/>
-      <div class="__content row">
+      <div class="__content flex flex-row">
         <div class="w-140 tempat__menu">
           <menu-draw :width="app.txtLg" @on-selected="handleMenu" />
         </div>
-        <div class="relative flex flex-1 bg-primary">
-          <div>gdsgds</div>
+        <div class="flex flex-1">
+          <PagePanels v-model="menu" />
         </div>
       </div>
     </div>
@@ -22,8 +22,11 @@ import { useAppStore } from 'src/stores/app'
 import { ref } from 'vue'
 import HeaderPage from './executive/HeaderPage.vue'
 import MenuDraw from './executive/MenuDraw.vue'
+import PagePanels from 'src/pages/executive/panels/IndexPage.vue'
 
 const app = useAppStore()
+// const widthMenu = ref('140px')
+// const widthContent = ref((app.width - 250) + 'px')
 const menu = ref('Keuangan')
 function handleMenu (val) {
   menu.value = val
---
+++ src/pages/executive/panels/IndexPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-tab-panels
+    animated
+    swipeable
+    vertical
+    infinite
+    class="full-width"
+  >
+    <q-tab-panel name="Keuangan">
+      <!-- <div class="text-h6">Keuangan</div> -->
+      <div class="flex flex-row">
+        <div class="flex w-1-4">
+          <q-card flat class="my-card-dark full-width">
+            <div class="q-pa-lg">
+              dasdas
+            </div>
+          </q-card>
+        </div>
+        <div class="flex flex-1 bg-primary">2</div>
+      </div>
+    </q-tab-panel>
+
+    <q-tab-panel name="SDM">
+      <div class="text-h6">SDM</div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </q-tab-panel>
+
+    <q-tab-panel name="Layanan">
+      <div class="text-h6">Layanan</div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </q-tab-panel>
+
+    <q-tab-panel name="Sarpras">
+      <div class="text-h6">Sarpras</div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </q-tab-panel>
+  </q-tab-panels>
+</template>
+
+<script setup>
+
+</script>
